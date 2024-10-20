d8cee968b1354e74b93118212550a79fbf9e3470
Prasidh Gopal Anchan <prasidhgopalanchan@gmail.com>
2024-10-20 16:53:51 +0530

    chore: add support for API level 28

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -14,7 +14,7 @@
 import org.gradle.api.JavaVersion
 
 object AndroidConfig {
-    const val MIN_SDK = 29
+    const val MIN_SDK = 28
     const val TARGET_SDK = 34
     const val COMPILE_SDK = 34
     val JAVA_VERSION = JavaVersion.VERSION_21
