0127e42603709427b68b2e7010779afe2919868d
Prasidh Gopal Anchan <prasidhgopalanchan@gmail.com>
2025-02-28 19:52:39 +0530

    chore: bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
             excludes += "META-INF/DEPENDENCIES"
+            excludes += "META-INF/INDEX.LIST"
         }
     }
 }
